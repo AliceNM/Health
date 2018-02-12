30d1e1aacbd7b32fcb63dca04b064684524145e4
Alicemaina <alicemainachege@gmail.com>
2018-02-12 18:58:02 +0300

    Nav

--- app/src/main/java/com/example/alice/health/MainActivity.java
+++ app/src/main/java/com/example/alice/health/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -80,17 +80,21 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_contacts) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_insurance) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_location) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.nav_medDetails) {
+
+        } else if (id == R.id.nav_userProfile) {
 
         }
 
